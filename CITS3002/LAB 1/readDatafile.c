baddf1270d51b76ccaaf7873a672bcb05a10210c
J Lumagbas <jadlumagbas@gmail.com>
2023-03-12 00:04:32 +0800

    fixed file opener code

--- CITS3002/LAB 1/readDatafile.c
+++ CITS3002/LAB 1/readDatafile.c
@@ -1,14 +1,18 @@
 
 #include <stdio.h>
 #include <stdlib.h> // For exit()
-  
+#include <fcntl.h>
 int main()
 {
-   unsigned char buffer[1024];
+    // Simple function to read Q1.DataFile
+    uint32_t op;
     FILE *ptr;
-
-    ptr = fopen("Q1.datafile","rb");  // r for read, b for binary
-    fread(buffer,sizeof(buffer),1,ptr); // read 10 bytes to our buffer
-    for(int i = -50; i<50; i++)
-    printf("%i\n", buffer[i]); // prints a series of bytes
+    ptr = fopen("Q1.datafile","rb+");  // r for read, b for binary
+    for(int32_t a = -50; a<=50; ++a)
+    {   
+        fread(&op,sizeof(op),1,ptr); // read 10 bytes to our buffer
+        printf("%4i\t0x%08x\t%10i\t0x%08x\n", a, a, op,op);
+    }
+    
+    
 }
